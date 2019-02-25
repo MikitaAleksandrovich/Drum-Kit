e39bb27d96854d552b6c960b1e9eb59188c03ac9
Mikita Aleksandrovich <45469739+MikitaAleksandrovich@users.noreply.github.com>
2019-02-25 13:23:29 +0300

    Replace some elements using jQuery

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 // Store the number of drum's buttons into variable 
 
-var numberOfDrumButtons = document.querySelectorAll('.drum').length;
+var numberOfDrumButtons = $('.drum').length;
 
 
 
@@ -9,23 +9,20 @@ var numberOfDrumButtons = document.querySelectorAll('.drum').length;
 
 for (var i = 0; i < numberOfDrumButtons; i ++) {
 
-    document.querySelectorAll('.drum')[i].addEventListener('click', function () {
+    $('.drum')[i].addEventListener('click', function () {
 
         // Select THIS of each querySelectorAll('.drum')[i] to use it in functions when clicked
         var buttonInnerHTML = this.innerHTML;
         
         // Call functions
         makeSound(buttonInnerHTML);
-        buttonAnimation(buttonInnerHTML);
-
-    
+		  buttonAnimation(buttonInnerHTML);
+		  
     });
 }
 
 
-
 // Call same functions when pressed drum's buttons (depending on key (w, a, s, d and etc))
-
 document.addEventListener('keypress', function (event) {
 
     makeSound(event.key);
@@ -34,108 +31,53 @@ document.addEventListener('keypress', function (event) {
 });
 
 
-
-
 function makeSound(key) {
-
-  
 	switch (key) {
-    
 	   case "w":
-      
-		var tom1 = new Audio("tom-1.mp3");
-      
-		tom1.play();
-      
-		break;
-
-    
+			var tom1 = new Audio("tom-1.mp3");
+			tom1.play();
+			break;
 	   case "a":
-      
-		var tom2 = new Audio("tom-2.mp3");
-      
-		tom2.play();
-      
-		break;
-
-    
+			var tom2 = new Audio("tom-2.mp3");
+			tom2.play();
+			break;
 	   case "s":
-      
-		var tom3 = new Audio('tom-3.mp3');
-      
-		tom3.play();
-      
-		break;
-
-    
+			var tom3 = new Audio('tom-3.mp3');
+			tom3.play();
+			break;
 	   case "d":
-      
-		var tom4 = new Audio('tom-4.mp3');
-      
-		tom4.play();
-      
-		break;
-
-    
+			var tom4 = new Audio('tom-4.mp3');
+			tom4.play();
+			break;
 	   case "j":
-      
-		var snare = new Audio('snare.mp3');
-      
-		snare.play();
-      
-		break;
-
-    
+			var snare = new Audio('snare.mp3');
+			snare.play();
+			break;
 	   case "k":
-      
-		var crash = new Audio('crash.mp3');
-      
-		crash.play();
-      
-		break;
-
-    
+			var crash = new Audio('crash.mp3');
+			crash.play();
+			break;
 	   case "l":
-      
-		var kick = new Audio('kick-bass.mp3');
-      
-		kick.play();
-     
-		break;
-
-
-    
+			var kick = new Audio('kick-bass.mp3');
+			kick.play();
+			break;
 	   default: 
-		console.log(key);
-
-  
+			console.log(key);
+	}
 }
-}
-
-
 
 
 // Add buttonAnimation function to change style of button when pressed it
-
-
 function buttonAnimation(currentKey) {
 
+	var activeButton = $("." + currentKey);
  
+	activeButton.addClass("pressed");
  
-	var activeButton = document.querySelector("." + currentKey);
-
-  
-	activeButton.classList.add("pressed");
-
-  
 	setTimeout(function() {
-    
-	activeButton.classList.remove("pressed");
-  }, 100);
-
-
-
-}
+	  activeButton.removeClass("pressed");
+	}, 100);
+ }
 
 
 
